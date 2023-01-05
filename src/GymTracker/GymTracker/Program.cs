6ac215cbbd54b4548303b4b7665bdafdf141dd5b
Ross Scott <ross.d.scott@gmail.com>
2023-01-05 14:16:12 +0000

    Clear out state, ready for next stage

--- src/GymTracker/GymTracker/Program.cs
+++ src/GymTracker/GymTracker/Program.cs
@@ -11,6 +11,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 
-builder.Services.AddScoped<CountdownTimerState>();
 
 await builder.Build().RunAsync();
