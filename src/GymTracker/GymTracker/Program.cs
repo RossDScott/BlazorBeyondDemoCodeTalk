abf63bac0eda95b1bb52ef46f89505d872740da2
Ross Scott <ross.d.scott@gmail.com>
2023-01-09 20:59:08 +0000

    Ready to use fluxor

--- src/GymTracker/GymTracker/Program.cs
+++ src/GymTracker/GymTracker/Program.cs
@@ -1,3 +1,4 @@
+using Fluxor;
 using GymTracker;
 using GymTracker.Features.Session;
 using Microsoft.AspNetCore.Components.Web;
@@ -10,6 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
-
+builder.Services.AddFluxor(o => o.ScanAssemblies(typeof(Program).Assembly).UseReduxDevTools());
 
 await builder.Build().RunAsync();
