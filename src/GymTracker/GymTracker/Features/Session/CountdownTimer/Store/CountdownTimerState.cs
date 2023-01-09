4dc502c6734dab8c04996070732e3b0acf6f2cf7
Ross Scott <ross.d.scott@gmail.com>
2023-01-09 21:18:37 +0000

    Fluxor state added

---
+++ src/GymTracker/GymTracker/Features/Session/CountdownTimer/Store/CountdownTimerState.cs
@@ -0,0 +1,11 @@
+﻿using Fluxor;
+
+namespace GymTracker.Features.Session.CountdownTimer.Store;
+
+[FeatureState]
+public record CountdownTimerState
+{
+    public DateTime? StartTime { get; init; } = null;
+    public TimeSpan StartDuration { get; init; } = TimeSpan.FromMilliseconds(2500);
+    public TimeSpan Duration { get; init; } = TimeSpan.FromMilliseconds(2500);
+}
