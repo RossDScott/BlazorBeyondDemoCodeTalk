c13c2ef8ceb60f16f6deecd326f7f6cefbf5d0a0
Ross Scott <ross.d.scott@gmail.com>
2023-01-10 18:34:55 +0000

    Actions & dispatchers wired up

---
+++ src/GymTracker/GymTracker/Features/Session/CountdownTimer/Store/CountdownTimerActions.cs
@@ -0,0 +1,5 @@
+﻿namespace GymTracker.Features.Session.CountdownTimer.Store;
+
+public record CountdownTimerStartAction(DateTime StartTime);
+public record CountdownTimerStartWithDurationAction(DateTime StartTime, TimeSpan Duration);
+public record CountdownTimerSetDuration(TimeSpan Duration);
