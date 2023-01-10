539dcb2bf87ca6706a27dfcbd40fe20a7236cd04
Ross Scott <ross.d.scott@gmail.com>
2023-01-10 18:51:59 +0000

    Reducers created

---
+++ src/GymTracker/GymTracker/Features/Session/CountdownTimer/Store/CountdownTimerReducer.cs
@@ -0,0 +1,34 @@
+﻿using Fluxor;
+
+namespace GymTracker.Features.Session.CountdownTimer.Store;
+
+public static class CountdownTimerReducer
+{
+    [ReducerMethod]
+    public static CountdownTimerState OnStart(CountdownTimerState state, CountdownTimerStartAction action)
+    {
+        return state with
+        {
+            StartTime = action.StartTime
+        };
+    }
+
+    [ReducerMethod]
+    public static CountdownTimerState OnStart(CountdownTimerState state, CountdownTimerStartWithDurationAction action)
+    {
+        return state with
+        {
+            StartTime = action.StartTime,
+            StartDuration = action.Duration
+        };
+    }
+
+    [ReducerMethod]
+    public static CountdownTimerState OnSetDuration(CountdownTimerState state, CountdownTimerSetDuration action)
+    {
+        return state with
+        {
+            Duration = action.Duration
+        };
+    }
+}
